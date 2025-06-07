7919ea9f05665df1effaec03ff0006f2d7396694
LipeSou <felipesoutomdt@gmail.com>
2025-06-07 17:36:30 -0300

    fix: CI type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         </h1>
 
         <PropertyFilters />
-        <PropertyList properties={properties} />
+        <PropertyList />
       </main>
     </>
   );
